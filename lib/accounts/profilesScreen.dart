66d122be3032c5effba563dc41bd0b9fee579a5c
jprincip21 <jprincip21@gmail.com>
2024-04-16 16:05:00 -0400

    profile screen styling Updated

--- lib/accounts/profilesScreen.dart
+++ lib/accounts/profilesScreen.dart
@@ -9,9 +9,14 @@ class Profiles extends StatefulWidget {
 class _ProfilesState extends State<Profiles> {
   @override
   Widget build(BuildContext context)  {
-    return Scaffold(appBar: AppBar(
-      backgroundColor: Colors.red,
-      title: Text("Profile Screen"),
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(
+      backgroundColor: Colors.purpleAccent,
+      centerTitle: true,
+      elevation: 10,
+      shadowColor: Colors.black,
+      title: Text("Profile Screen", style: TextStyle(color: Colors.white),),
     ),);
   }
 }
