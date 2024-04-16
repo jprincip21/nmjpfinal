61ab1b411257f6db428ade09bf9da30641b6e71d
jprincip21 <jprincip21@gmail.com>
2024-04-16 16:29:10 -0400

    Login Screen Updated

--- lib/accounts/loginScreen.dart
+++ lib/accounts/loginScreen.dart
@@ -9,6 +9,15 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context)  {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.purpleAccent,
+        elevation: 10,
+        shadowColor: Colors.black,
+        title: Text("Login Screen", style: TextStyle(color: Colors.white),)
+      ),
+    );
   }
 }
