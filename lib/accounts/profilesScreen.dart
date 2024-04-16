c728e465dd3c6ebd6b1a66f51ec52ed1d0f7cb35
nmulas <nicholasmulas12@gmail.com>
2024-04-16 16:39:36 -0400

    profile screen styling Updated
    (cherry picked from commit 05038edd5868840e977bc3d6bde463c549e5ccf7)

--- lib/accounts/profilesScreen.dart
+++ lib/accounts/profilesScreen.dart
@@ -1,3 +1,4 @@
+//profile screenn
 import 'package:flutter/material.dart';
 
 class Profiles extends StatefulWidget {
@@ -9,9 +10,14 @@ class Profiles extends StatefulWidget {
 class _ProfilesState extends State<Profiles> {
   @override
   Widget build(BuildContext context)  {
-    return Scaffold(appBar: AppBar(
-      backgroundColor: Colors.red,
-      title: Text("Profile Screen"),
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      appBar: AppBar(
+      backgroundColor: Colors.purpleAccent,
+      centerTitle: true,
+      elevation: 10,
+      shadowColor: Colors.black,
+      title: Text("Profile Screen", style: TextStyle(color: Colors.white),),
     ),);
   }
 }
