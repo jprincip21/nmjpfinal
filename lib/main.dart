922e175f01d0b0b3fa86ff9787b7b51990dbe88d
jprincip21 <jprincip21@gmail.com>
2024-04-17 15:11:32 -0400

    FireStore Initalization + Pubspec Changes + Pokemon Class
    (cherry picked from commit 5f411e89d9f09cd7f21e383b84d102bb1acb630b)

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 //Final Project Mobile Embedded Devices CENG10021
 //April 16 2024
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -18,6 +19,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true
   );
   runApp(
       MaterialApp(initialRoute: '/login',
